7e721ecad4d209735e6af45632d34c6e79b1d87b
Jorn van Bree <jornvbree@live.nl>
2024-07-26 11:22:49 +0200

    blal

--- src/pages/RecipeListPage.jsx
+++ src/pages/RecipeListPage.jsx
@@ -1,23 +1,17 @@
 import { Center, Heading, Flex } from "@chakra-ui/react";
 import { data } from "../utils/data";
 import { RecipeItem } from "../components/RecipeItem";
+//console.log(data);
 
-export const RecipeListPage = (/*{ recipes , clickFn }*/) => {
+export const RecipeListPage = (/*{ recipesb, clickFn }*/) => {
   // You can play around with the console log, but ultimately remove it once you are done
   // console.log(data.hits[2].recipe.label);
-  /*return (
-    <Flex gap={16} w="50%" flexWrap="wrap" justify={"center"}>
-      {recipes.map((recipe) => (
-        <RecipeItem key={recipe.id} recipe={recipe} clickFn={clickFn} />
-      ))}
-    </Flex>
-  );
-};*/
   return (
     <Center h="100vh" flexDir="column">
       <Heading>Your Recipe App</Heading>
       <div>
         <h1>Recipe List</h1>
+
         <ul>
           {data.map((recipe) => (
             <li key={recipe.label}>
@@ -31,15 +25,6 @@ export const RecipeListPage = (/*{ recipes , clickFn }*/) => {
               {recipe.cautions && <p>Cautions: {recipe.cautions.join(", ")}</p>}
               <p>Meal Type: {recipe.mealType}</p>
               <p>Dish Type:{recipe.dishType}</p>
-              <div>
-                Health Labels:
-                <ul>
-                  {recipe.healthLabels.includes("Vegan") && <li>Vegan</li>}
-                  {recipe.healthLabels.includes("Vegetarian") && (
-                    <li>Vegetarian</li>
-                  )}
-                </ul>
-              </div>
             </li>
           ))}
         </ul>
